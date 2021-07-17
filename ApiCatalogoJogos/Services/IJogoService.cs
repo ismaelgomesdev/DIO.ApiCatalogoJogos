c508d68a5957d16b64bbaac4bc46899782f6883d
Ismael Gomes <ismaelgomes@alu.ufc.br>
2021-07-16 21:35:31 -0300

    API finalizada

---
+++ ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,27 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+
+        Task<JogoViewModel> Obter(Guid idJogo);
+
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+
+        Task Atualizar(Guid idJogo, JogoInputModel jogo);
+
+        Task Atualizar(Guid idJogo, double preco);
+
+        Task Remover(Guid idJogo);
+
+
+    }
+}
